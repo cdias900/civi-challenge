3babec276c8c535bb28b7656f75074dc103f650b
Pedro Campos <pedro@cdias.dev>
2021-09-17 00:11:27 -0300

    fix: api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://civi-back-challenge.herokuapp.com',
-  baseURL: 'https://3e9d-189-61-54-7.ngrok.io',
+  baseURL: 'https://civi-back-challenge.herokuapp.com',
 });
